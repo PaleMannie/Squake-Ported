ab855ff0a6c8572ec680e26fa44bf5a77b97fde0
PaleMannie <einalex44@gmail.com>
2025-12-16 22:34:16 +0100

    Port 1.21.11 start

--- src/main/java/mett/palemannie/squakeported/util/KeyBinding.java
+++ src/main/java/mett/palemannie/squakeported/util/KeyBinding.java
@@ -2,7 +2,7 @@ package mett.palemannie.squakeported.util;
 
 import com.mojang.blaze3d.platform.InputConstants;
 import net.minecraft.client.KeyMapping;
-import net.minecraft.resources.ResourceLocation;
+import net.minecraft.resources.Identifier;
 import net.minecraftforge.client.settings.KeyConflictContext;
 import org.lwjgl.glfw.GLFW;
 
@@ -11,5 +11,5 @@ public class KeyBinding {
     public static final String KEY_TOGGLE_SQUAKEMODE = "squake.key.toggle";
 
     public static final KeyMapping SQUAKE_TOGGLE_KEY = new KeyMapping(KEY_TOGGLE_SQUAKEMODE, KeyConflictContext.IN_GAME,
-            InputConstants.Type.KEYSYM, GLFW.GLFW_KEY_COMMA, KeyMapping.Category.register(ResourceLocation.parse(KEY_CATEGORY_SQUAKE)));
+            InputConstants.Type.KEYSYM, GLFW.GLFW_KEY_COMMA, KeyMapping.Category.register(Identifier.parse(KEY_CATEGORY_SQUAKE)), 0);
     }
