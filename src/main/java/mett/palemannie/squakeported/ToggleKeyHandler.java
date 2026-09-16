dcb7644ab24b43c5c81881a0c07dfc510f6787b1
PaleMannie <einalex44@gmail.com>
2026-09-16 14:25:26 +0200

    hurt momentum break fix, new versioning, mod-id change

--- src/main/java/mett/palemannie/squakeported/ToggleKeyHandler.java
+++ src/main/java/mett/palemannie/squakeported/ToggleKeyHandler.java
@@ -1,4 +1,4 @@
-package mett.palemannie.squakeport_1_21_5;
+package mett.palemannie.squakeported;
 
 import net.minecraft.ChatFormatting;
 import net.minecraft.client.KeyMapping;
--- src/main/java/mett/palemannie/squakeported/mixins/MixinPlayerEntity.java
+++ src/main/java/mett/palemannie/squakeported/mixins/MixinPlayerEntity.java
@@ -1,7 +1,7 @@
-package mett.palemannie.squakeport_1_21_5.mixins;
+package mett.palemannie.squakeported.mixins;
 
-import mett.palemannie.squakeport_1_21_5.ISquakeEntity;
-import mett.palemannie.squakeport_1_21_5.SquakeClientPlayer;
+import mett.palemannie.squakeported.ISquakeEntity;
+import mett.palemannie.squakeported.SquakeClientPlayer;
 import net.minecraft.world.damagesource.DamageSource;
 import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.LivingEntity;
